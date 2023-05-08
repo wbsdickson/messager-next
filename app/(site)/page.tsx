370715934d9a9d6063c510e340409f904c6e7db9
Dickson Wu <wbs.dickson@gmail.com>
2023-05-08 02:33:38 -0600

    create auth form and social auth

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 export default function Home() {
     return (
         <div
@@ -11,7 +13,23 @@ export default function Home() {
             lg:px-8
             bg-gray-100"
         >
-            <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <Image alt="logo" height="48" width="48" className="mx-auto w-auto" src="/images/logo.png"></Image>
+                <h2
+                    className="
+                mt-6
+                text-center
+                text-3xl
+                font-bold
+                tracking-tight
+                text-gray-900
+                "
+                >
+                    Sign in to yout account
+                </h2>
+            </div>
+
+            <AuthForm />
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,9 @@ module.exports = {
     extend: {
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'class'
+    })
+  ],
 }
