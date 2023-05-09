670a8e57427b92c0f02eb8e9b9ab803a1abdedc3
Dickson Wu <wbs.dickson@gmail.com>
2023-05-09 03:08:44 -0600

    add google login

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -158,7 +158,7 @@ const AuthFrom = () => {
                         <AuthSocialButton
                             icon={BsGoogle}
                             onClick={() => {
-                                socialAction("github");
+                                socialAction("google");
                             }}
                         />
                     </div>
