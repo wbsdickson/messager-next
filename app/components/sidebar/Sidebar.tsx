1db35767a1d634068ae47ec58ae73216e3728a32
Dickson Wu <wbs.dickson@gmail.com>
2023-05-11 00:43:58 -0600

    add sidebar desktop and mobile

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 interface SidebarProps {}
 const Sidebar = async ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="h-full ">
             <DesktopSidebar />
+            <MobileFooter />
             <main className="lg:pl-20 h-full">{children}</main>
         </div>
     );
